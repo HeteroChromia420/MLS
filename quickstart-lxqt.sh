9c1b16bc625da7dedfaf98801e2038095188cc18
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2021-05-13 17:20:01 +0100

    Update quickstart-lxqt.sh

--- quickstart-lxqt.sh
+++ quickstart-lxqt.sh
@@ -9,8 +9,9 @@ curl -s 'https://download.opensuse.org/repositories/home:/ungoogled_chromium/Deb
 /bin/bash -c "$(curl -sL https://git.io/vokNn)"
 sudo apt-fast install lightdm lightdm-gtk-greeter-settings smplayer cantata mpd mpv youtube-dl synaptic eject bash-completion gvfs* qt5-style-plugins network-manager network-manager-gnome xfce4-screenshooter xinit pulseaudio alsa-utils ark xserver-xorg xdg-user-dirs wpasupplicant gimp xserver-xorg-video-intel firmware-amd-graphics fonts-noto* redshift-gtk geoclue-2.0 arc-theme kdeconnect papirus-icon-theme breeze-cursor-theme steam flatpak desktop-base caffeine xscreensaver lxqt-core kwrite lximage-qt lxqt-powermanagement lxqt-sudo pavucontrol-qt qps lm-sensors linux-cpupower tlp powertop xfce4-notifyd xfwm4 kcalc package-update-indicator plank --no-install-recommends --no-install-suggests -y
 xdg-user-dirs-update
-cp .mpdconf ~/
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install adriconf
 sudo apt-fast install -y openvpn dialog python3-pip python3-setuptools -y --no-install-recommends --no-install-suggests
 sudo pip3 install protonvpn-cli
+sudo cp 50-mouse-acceleration.conf /usr/share/X11/xorg.conf.d/
+cp .mpdconf ~/
