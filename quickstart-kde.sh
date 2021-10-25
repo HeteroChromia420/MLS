8eb69c010916c26fbd5c95b0de622cddaab5e567
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2021-10-25 22:46:50 +0100

    Update quickstart-kde.sh

--- quickstart-kde.sh
+++ quickstart-kde.sh
@@ -6,7 +6,7 @@ sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install curl aria2 wget ca-certificates --no-install-recommends --no-install-suggests -y
 /bin/bash -c "$(curl -sL https://git.io/vokNn)"
-sudo apt-fast install sddm smplayer cantata mpd mpv youtube-dl synaptic eject bash-completion gvfs* qt5-style-plugins plasma-nm kde-spectacle xinit pulseaudio alsa-utils ark gwenview kate xserver-xorg xdg-user-dirs wpasupplicant gimp xserver-xorg-video-intel firmware-amd-graphics fonts-noto* redshift-gtk geoclue-2.0 arc-theme kdeconnect papirus-icon-theme breeze-cursor-theme steam flatpak desktop-base kde-config-gtk-style kde-config-screenlocker kde-config-sddm khotkeys kinfocenter kmenuedit kscreen ksysguard kwin-x11 powerdevil systemsettings plasma-discover kde-plasma-desktop plasma-pa kwalletmanager qml-module-org-kde-newstuff debian-pulseaudio-config-override latte-dock qml-module-org-kde-activities kactivities-bin --no-install-recommends --no-install-suggests -y
+sudo apt-fast install firefox-esr sddm smplayer cantata mpd mpv youtube-dl synaptic eject bash-completion gvfs* qt5-style-plugins plasma-nm kde-spectacle xinit pulseaudio alsa-utils ark gwenview kate xserver-xorg xdg-user-dirs wpasupplicant gimp xserver-xorg-video-nouveau fonts-noto* geoclue-2.0 arc-theme kdeconnect papirus-icon-theme breeze-cursor-theme steam flatpak desktop-base kde-config-gtk-style kde-config-screenlocker kde-config-sddm khotkeys kinfocenter kmenuedit kscreen ksysguard kio-extras kwin-x11 powerdevil systemsettings plasma-discover kde-plasma-desktop plasma-pa kwalletmanager qml-module-org-kde-newstuff qml-module-org-kde-activities kactivities-bin --no-install-recommends --no-install-suggests -y
 xdg-user-dirs-update
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install adriconf
@@ -14,7 +14,3 @@ sudo apt-fast install -y openvpn dialog python3-pip python3-setuptools -y --no-i
 sudo pip3 install protonvpn-cli
 sudo cp 50-mouse-acceleration.conf /usr/share/X11/xorg.conf.d/
 cp .mpdconf ~/
-echo 'deb http://download.opensuse.org/repositories/home:/ungoogled_chromium/Debian_Buster/ /' | sudo tee /etc/apt/sources.list.d/home-ungoogled_chromium.list
-curl -s 'https://download.opensuse.org/repositories/home:/ungoogled_chromium/Debian_Buster/Release.key' | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home-ungoogled_chromium.gpg 
-sudo apt update
-sudo apt-fast install -y ungoogled-chromium ungoogled-chromium-sandbox --no-install-recommends
