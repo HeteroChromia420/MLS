63df3b00b79da16d7672d64d55410fd36fbdaa34
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2021-11-09 21:09:58 +0000

    Update mintpostinst.sh

--- mintpostinst.sh
+++ mintpostinst.sh
@@ -4,7 +4,7 @@ clear
 
 # in this sh file, there's just a bunch of packages that i personally use.
 sudo apt update
-sudo apt purge --auto-remove firefox libreoffice* thunderbird onboard catfish sticky celluoid compiz* drawing hexchat hypnotix light-locker redshift* metacity* rhythmbox transmission-* warpinator sticky catfish orca
+sudo apt purge --auto-remove firefox libreoffice* thunderbird onboard catfish sticky celluloid compiz* drawing hexchat hypnotix light-locker redshift* metacity* rhythmbox transmission-* warpinator sticky catfish orca
 sudo apt install plank cantata gimp fonts-noto* kdeconnect breeze-cursor-theme steam plank mpv mpd xscreensaver caffeine -y --no-install-recommends --no-install-suggests 
 sudo cp 50-mouse-acceleration.conf /usr/share/X11/xorg.conf.d/
 sudo cp 69-sysctl-performance.conf /etc/sysctl.d/
