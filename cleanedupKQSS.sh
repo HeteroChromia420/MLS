f5475cf33f60e79b12b7145e10aa54eef040acaf
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2022-07-23 21:15:48 +0000

    Experimental

---
+++ cleanedupKQSS.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+clearScreen(){
+    clear
+}
+
+updateSystem(){
+    sudo apt update
+    clearScreen
+}
+
+installPackages(){
+    sudo apt install firefox-esr smplayer cantata mpd mpv youtube-dl synaptic eject bash-completion gvfs* plasma-nm kde-spectacle xinit pulseaudio alsa-utils ark gwenview kate xserver-xorg xdg-user-dirs wpasupplicant gimp xserver-xorg-video-nouveau fonts-noto* geoclue-2.0 arc-theme kdeconnect papirus-icon-theme breeze-cursor-theme flatpak desktop-base kde-config-gtk-style kde-config-screenlocker khotkeys kinfocenter kmenuedit kscreen qps kio-extras kwin-x11 powerdevil systemsettings plasma-discover kde-plasma-desktop plasma-pa kwalletmanager qml-module-org-kde-newstuff qml-module-org-kde-activities kactivities-bin ffmpegthumbs lightdm lightdm-gtk-greeter-settings  --no-install-recommends --no-install-suggests -y --no-install-recommends --no-install-suggests -y
+    clearScreen
+}
+
+setupFlatpak(){
+    sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    clearScreen
+}
+
+updateSystem && installPackages && setupFlatpak && echo "All compents ran successfully!" || echo "One or More Components failed to run."
+echo "Process has completed."
+read -n 1 -s -r -p "Press any key to continue..."
