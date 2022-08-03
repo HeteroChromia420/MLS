c3ecb70c8d158a4325376b09db9e383ff34cdc5f
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2022-08-03 14:01:07 +0000

    Add files via upload

--- cleanedupKQSS.sh
+++ cleanedupKQSS.sh
@@ -10,7 +10,8 @@ updateSystem(){
 }
 
 installPackages(){
-    sudo apt install firefox-esr smplayer cantata mpd mpv youtube-dl synaptic eject bash-completion gvfs* plasma-nm kde-spectacle xinit pulseaudio alsa-utils ark gwenview kate xserver-xorg xdg-user-dirs wpasupplicant gimp xserver-xorg-video-nouveau fonts-noto* geoclue-2.0 arc-theme kdeconnect papirus-icon-theme breeze-cursor-theme flatpak desktop-base kde-config-gtk-style kde-config-screenlocker khotkeys kinfocenter kmenuedit kscreen qps kio-extras kwin-x11 powerdevil systemsettings plasma-discover kde-plasma-desktop plasma-pa kwalletmanager qml-module-org-kde-newstuff qml-module-org-kde-activities kactivities-bin ffmpegthumbs lightdm lightdm-gtk-greeter-settings  --no-install-recommends --no-install-suggests -y --no-install-recommends --no-install-suggests -y
+    sudo apt install firefox-esr smplayer cantata mpd mpv youtube-dl synaptic eject bash-completion gvfs* plasma-nm kde-spectacle xinit pulseaudio alsa-utils ark gwenview kate xserver-xorg xdg-user-dirs wpasupplicant gimp xserver-xorg-video-nouveau fonts-noto* geoclue-2.0 arc-theme kdeconnect papirus-icon-theme breeze-cursor-theme desktop-base kde-config-gtk-style kde-config-screenlocker khotkeys kinfocenter kmenuedit kscreen qps kio-extras kwin-x11 powerdevil systemsettings kde-plasma-desktop plasma-pa kwalletmanager qml-module-org-kde-newstuff qml-module-org-kde-activities kactivities-bin ffmpegthumbs lightdm lightdm-gtk-greeter-settings  --no-install-recommends --no-install-suggests -y --no-install-recommends --no-install-suggests -y
+    sudo apt install --instal-recommends plasma-discover plasma-discover-backend-flatpak flatpak -y
     clearScreen
 }
 
@@ -20,5 +21,4 @@ setupFlatpak(){
 }
 
 updateSystem && installPackages && setupFlatpak && echo "All compents ran successfully!" || echo "One or More Components failed to run."
-echo "Process has completed."
-read -n 1 -s -r -p "Press any key to continue..."
+read -n 1 -s -r -p "Press any key to exit..."
