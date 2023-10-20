93d98d952da3b021031aab5a98066f8f1399a17e
HeteroChromia420 <56137935+HeteroChromia420@users.noreply.github.com>
2023-10-21 00:37:00 +0100

    Update spiral-linux-postsetup.sh

--- spiral-linux-postsetup.sh
+++ spiral-linux-postsetup.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 sudo apt update
-sudo apt purge --auto-remove catfish gnome-software* lxqt-archiver mousepad xfce4-* simple-scan evince clementine vlc drawing libreoffice-* shotwell pidgin thunderbird transmission-* hplip*
-sudo apt install featherpad kdeconnect xcompmgr xscreensaver geoclue-2.0 qps flatpak
+sudo apt purge --auto-remove parcellite catfish gnome-software* lxqt-archiver mousepad xfce4-* simple-scan evince clementine vlc drawing libreoffice-* shotwell pidgin thunderbird transmission-* hplip*
+sudo apt install featherpad kdeconnect xcompmgr xscreensaver geoclue-2.0 qps flatpak redshift-gtk flameshot engrampa
 sudo apt install --install-recommends plasma-discover plasma-discover-backend-flatpak 
 sudo flatpak remote-delete flathub --system
 flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
