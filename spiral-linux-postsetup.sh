adb8c2bf1bb4da657305b5efa8b1b70871539540
HeteroChromia420 <56137935+HeteroChromia420@users.noreply.github.com>
2023-10-23 13:46:38 +0100

    Update spiral-linux-postsetup.sh

--- spiral-linux-postsetup.sh
+++ spiral-linux-postsetup.sh
@@ -8,3 +8,12 @@ sudo flatpak remote-delete flathub --system
 flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo apt dist-upgrade --auto-remove
 flatpak update
+git clone https://github.com/catppuccin/lxqt
+git clone https://github.com/catppuccin/openbox
+git clone https://github.com/catppuccin/plank
+sudo mv lxqt/src/catppuccin-mocha /usr/share/lxqt/themes
+mkdir ~/.themes
+mv openbox/Catppuccin-Mocha ~/.themes
+mkdir ~/.local/share/plank
+mkdir ~/.local/share/plank/themes
+mv plank/src/Catppuccin-mocha ~/.local/share/plank/themes
