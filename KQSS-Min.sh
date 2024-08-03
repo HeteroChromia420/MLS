39e39762914d1d3783ca982ee3428212626b1df7
HeteroChromia420 <56137935+HeteroChromia420@users.noreply.github.com>
2024-08-03 21:12:09 +0100

    Update KQSS-Min.sh

--- KQSS-Min.sh
+++ KQSS-Min.sh
@@ -11,7 +11,7 @@ updateSystem(){
 }
 
 installPackages(){
-    sudo apt install synaptic eject bash-completion gvfs* plasma-nm kde-spectacle xinit pulseaudio alsa-utils ark gwenview kate xserver-xorg xdg-user-dirs wpasupplicant arc-theme papirus-icon-theme breeze-cursor-theme desktop-base kde-config-gtk-style kde-config-screenlocker khotkeys kinfocenter kmenuedit kscreen qps kio-extras kwin-x11 powerdevil systemsettings kde-plasma-desktop plasma-pa kwalletmanager qml-module-org-kde-newstuff qml-module-org-kde-activities kactivities-bin ffmpegthumbs sddm kde-config-sddm --no-install-recommends --no-install-suggests -y | tee -a kqss-setup.log
+    sudo apt install libnotify-bin kdeplasma-addons-data sddm-theme-breeze synaptic eject bash-completion gvfs* plasma-nm kde-spectacle xinit pulseaudio alsa-utils ark gwenview kate xserver-xorg xdg-user-dirs wpasupplicant arc-theme papirus-icon-theme breeze-cursor-theme desktop-base kde-config-gtk-style kde-config-screenlocker khotkeys kinfocenter kmenuedit kscreen qps kio-extras kwin-x11 powerdevil systemsettings kde-plasma-desktop plasma-pa kwalletmanager qml-module-org-kde-newstuff qml-module-org-kde-activities kactivities-bin ffmpegthumbs sddm kde-config-sddm --no-install-recommends --no-install-suggests -y | tee -a kqss-setup.log
     sudo apt install --install-recommends plasma-discover plasma-discover-backend-flatpak flatpak -y
     #And Purge unwanted packages
     sudo apt purge --auto-remove unattended-upgrades snapd plasma-discover-backend-snap -y | tee -a kqss-setup.log
